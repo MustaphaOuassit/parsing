620cb986514c0defa3aaed13fb5058dcd72b9c18
Mustapha Ouassit <mouassit@e1r13p12.1337.ma>
2021-11-21 20:37:27 +0100

    add

--- srcs/parsing/linkedlist.c
+++ srcs/parsing/linkedlist.c
@@ -32,11 +32,65 @@ int    list_tokens(t_list **head, char *data)
 return(0);   
 }
 
+int	check_dividers(int value, int *type)
+{
+	if(value == '|' || value == '>' || value == '<')
+	{
+		if (value == '|')
+			*type = 1;
+		if(value == '>')
+			*type = 2;
+		if(value == '<')
+			*type = 3;
+		return(1);
+		
+	}
+	return(0);
+}
+
+void	put_in_parcer(char *value, int type)
+{
+	printf("V : %s T : %d\n",value,type);
+}
+
 int    check_tokens(t_list *head, int error)
 {
+	int i;
+
+	i = 0;
     while (head != NULL)
     {
-        printf("%s\n",head->value);
+		int type;
+		char *dividers;
+
+		i = 0;
+		dividers = (char *)malloc(sizeof(char) * (2));
+		while (head->value[i])
+		{
+			if(check_dividers(head->value[i],&type))
+			{
+				if((head->value[i] == '>' && head->value[i + 1] && head->value[i + 1] == '>'))
+				{
+					dividers = ft_strdup(">>");
+					type = 4;
+					i++;
+				}
+				else if((head->value[i] == '<' && head->value[i + 1] && head->value[i + 1] == '<'))
+				{
+					dividers = ft_strdup("<<");
+					type = 5;
+					i++;
+				}
+				else
+				{
+					dividers[0] = head->value[i];
+					dividers[1] = '\0';
+				}
+				put_in_parcer(dividers,type);
+			}
+			i++;
+		}
+		
         head = head->next;
     }
     error = 2;
