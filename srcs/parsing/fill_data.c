6207467e02a71b63a2af2ba114c0771b94b34101
Mustapha Ouassit <mouassit@e1r13p10.1337.ma>
2021-11-27 14:40:18 +0100

    add to arguments

--- srcs/parsing/fill_data.c
+++ srcs/parsing/fill_data.c
@@ -35,46 +35,257 @@ int	redirection_token(t_redirection	**head,int type, char *file_name)
     return(0);  
 }
 
-int	args_token(t_args **head, char *args)
+char	*convert(char *value)
 {
-	t_args *new_node = malloc(sizeof(t_redirection));
-	t_args *line;
+	char *error;
+	int i;
+	int j;
 
-	line = *head;
-	new_node->arguments = args;
-	new_node->next = NULL;
-	if(*head == NULL)
+	i = 0;
+	j = 0;
+	error = (char *)malloc(sizeof(char) * ((int)ft_strlen(value) + 3));
+	error[(int)ft_strlen(value) + 2] = '\0';
+	while (error[i])
 	{
-		*head = new_node;
-		return(0);
+		if(i == 0)
+		{
+			error[i] = '`';
+			i++;
+		}
+		else if(i == (int)ft_strlen(error) - 1)
+		{
+			error[i] = '\'';
+			i++;
+		}
+		else
+		{
+			error[i] = value[j];
+			j++;
+			i++;
+		}
 	}
-	while (line->next != NULL)
+	return(error);
+}
+
+void	print_error_rdt(char *value)
+{
+	char	*error;
+	char	*error_rdt;
+
+	error = "minishell: syntax error near unexpected token ";
+	error_rdt = NULL;
+	error_rdt = convert(value);
+	error = ft_strjoin(error,error_rdt);
+	write(1,error,ft_strlen(error));
+	write(1,"\n",1);
+}
+
+int		error_redirection(int check, t_tokens *tokens)
+{
+	if(check == 1 || tokens->next == NULL)
 	{
-		line = line->next;
+		if(check == 1)
+			print_error_rdt(tokens->value);
+		else
+			print_error_rdt("newline");
+		return(1);
 	}
-	line->next = new_node;
-    return(0);  
+	return(0);
+}
+
+int		get_len_double(char *value)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (i <= (int)ft_strlen(value))
+	{
+		if(value[i] == '\"' )
+			len++;
+		i++;
+	}
+	return(len / 2);
+}
+int		get_allocation(char *value)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (i <= (int)ft_strlen(value))
+	{
+		if(value[i] == ' ' )
+		{
+			i++;
+			while (value[i])
+			{
+				if(value[i] != ' ')
+				{
+					len++;
+					break;
+				}
+				i++;
+			}
+		}
+		else if(value[i] == '\"' )
+		{
+			i++;
+			while (value[i] != '\"')
+				i++;
+		}
+		i++;
+	}
+	if(len == 0)
+		len = 1;
+	return(len);
+}
+
+int		len_args(char *value ,int *start)
+{
+	int len;
+	int check;
+
+	len = 0;
+	check = 0;
+	while (value[*start])
+	{
+		if(value[*start] == '\"')
+		{
+			*start = *start + 1;
+			while (value[*start])
+			{
+				if(value[*start] == '\"')
+				{
+					break;
+				}
+				len++;
+				*start = *start + 1;
+			}
+		}
+		if(value[*start] != ' ')
+		{
+			if(value[*start] != '\"')
+			{
+				check = 1;
+				len++;
+			}
+		}
+		else if(value[*start] == ' ' && check == 1)
+			break;
+		*start = *start + 1;
+	}
+	return(len);
+}
+
+char  **filter_args(char *value)
+{
+	int j;
+	int i;
+	int r;
+	int len;
+	char **filter;
+	int tmp;
+
+	j = 0;
+	i = 0;
+	r = 0;
+	filter = NULL;
+	len = get_allocation(value);
+	if(len)
+	{
+		filter = (char **)malloc(sizeof(char *) * (len + 1));
+		filter[len] = 0;
+		j = 0;
+		tmp = 0;
+		while (j <= (int)ft_strlen(value))
+		{
+			i = len_args(value,&j);
+			filter[r] = (char *)malloc(sizeof(char) * (i + 1));
+			filter[r][i] = '\0';
+			i = 0;
+			while (filter[r][i])
+			{
+				if(value[tmp] == '\"')
+				{
+					tmp++;
+					while (value[tmp])
+					{
+						if(value[tmp] == '\"')
+							break;
+						filter[r][i] = value[tmp];
+						i++;
+						tmp++;
+					}
+				}
+				else if(value[tmp] != ' ')
+				{
+					filter[r][i] = value[tmp];
+					i++;
+					tmp++;
+				}
+				else
+					tmp++;
+			}
+			r++;
+			j++;
+		}
+	}
+	return(filter);
 }
 
 int     fill_data(t_tokens *tokens)
 {
-    t_data *data;
+    t_redirection *rdt;
     t_args *args;
+	int		check;
+	int		type;
+	int 	j;
+	char **filter;
 
-    data = NULL;
+    rdt = NULL;
     args = NULL;
-    data->redirection = NULL;
-    data->arguments = NULL;
+	check = 0;
+	j = 0;
     while (tokens != NULL)
     {
-        if( tokens->type <= 5 && tokens->type >= 2)
-        {
-            redirection_token(&data->redirection, tokens->value,tokens->type);
-        }
-        else
-            args_token(&args,tokens->value);
+		if( tokens->type <= 5 && tokens->type >= 2)
+		{
+			if(error_redirection(check, tokens))
+				return(1);
+			check = 1;
+			type = tokens->type;
+		}
+		else if(check == 1)
+		{
+			check = 0;
+			redirection_token(&rdt,type,tokens->value);
+		}
+		else
+		{
+			filter = filter_args(tokens->value);
+			if(filter)
+			{
+				j = 0;
+				while (filter[j])
+				{
+					printf("**%s**\n",filter[j]);
+					j++;
+				}
+				
+			}
+		}
         tokens = tokens->next;
     }
+
+	while (rdt != NULL)
+	{
+		printf("%d %s\n",rdt->type,rdt->file_name);
+		rdt = rdt->next;
+	}
+	
     
     return(0);
 }
